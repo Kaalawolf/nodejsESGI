239ca5e5b4958051714992ef960996bc829b3cd8
Mégane Dabin <dabinmegane@gmail.com>
2019-09-25 12:23:55 +0200

    add /hello and name

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/hello', function (req, res) {
+  if(req.query.name==null)
+  	res.send('Quel est votre nom ?')
+  else
+  	res.send('Bonjour, ' + req.query.name + " !")
+})
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!')
 })
